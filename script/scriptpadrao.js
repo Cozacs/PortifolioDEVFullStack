7f9b9757b7079bb9d6988925e1e2228fed05719d
Cozac <victorcozac32@gmail.com>
2023-09-04 14:03:57 -0300

    Celular pequeno Layout
    sem query apenas Celular pequeno

---
+++ script/scriptpadrao.js
@@ -0,0 +1,29 @@
+function mouseongit() {
+    var imagem = document.getElementById('git');
+    imagem.src = "imagens/GitHubLogo-hover.png";
+}
+
+function mouseoffgit() {
+    var imagem = document.getElementById('git');
+    imagem.src = "imagens/GitHubLogo.png";
+}
+
+function mouseonlinked() {
+    var imagem = document.getElementById('linked');
+    imagem.src = "imagens/LinkedinLogo-hover.png"
+}
+
+function mouseofflinked() {
+    var imagem = document.getElementById('linked');
+    imagem.src = "imagens/LinkedinLogo.png";
+}
+
+function mouseonmail() {
+    var imagem = document.getElementById('mail');
+    imagem.src = "imagens/MailLogo-hover.png";
+}
+
+function mouseoffmail() {
+    var imagem = document.getElementById('mail');
+    imagem.src = "imagens/MailLogo.png";
+}
